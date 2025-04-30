a6e4e39f129ee322c158b2aed75225ef0757dec4
udothompson81@gmail.com <your.email@example.com>
2025-04-30 04:42:04 +0100

    new commit

--- docs/script/index.js
+++ docs/script/index.js
@@ -12,3 +12,27 @@ bar.addEventListener('click', ()=>{
     }
     nav.classList.toggle('toggle')
 })
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const flipper = document.getElementById('flipper');
+//     const heroImg = document.getElementById('hero_img');
+
+//     // Create the back image dynamically
+//     const backImage = document.createElement('img');
+//     backImage.src = 'assets/images/RF_HK-guide_SQ_20250318a.jpg';
+//     backImage.alt = 'Back Image';
+//     backImage.classList.add('back');
+
+//     // Append the back image to the flipper
+//     flipper.appendChild(backImage);
+
+//     // Flip logic: toggle between two images
+//     let flipped = false;
+//     setInterval(() => {
+//       // Flip the container
+//       flipped = !flipped;
+//       flipper.classList.toggle('flip', flipped);
+//     }, 3000); // flip every 1 second
+//   });
